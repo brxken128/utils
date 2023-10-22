b780a037a02ed5cfc9165bbba9d18c17d190c149
jake <77554505+brxken128@users.noreply.github.com>
2023-10-22 08:20:54 +0100

    zeroize: feature gating again

--- zeroize/src/x86.rs
+++ zeroize/src/x86.rs
@@ -28,7 +28,8 @@ macro_rules! impl_zeroize_for_simd_register {
 
     ($feature:expr, $($type:ty),*) => {
         $(
-            #[cfg_attr(docsrs, doc(cfg(all(any(target_arch = "x86", target_arch = "x86_64"), feature = $feature))))]
+            #[cfg_attr(docsrs, doc(cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = $feature))))]
+            #[cfg(target_feature = $feature)]
             impl Zeroize for $type {
                 #[inline]
                 fn zeroize(&mut self) {
