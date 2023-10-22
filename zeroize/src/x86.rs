fbee0d01b2346e8cd74f56f825906eb50fb64842
jake <77554505+brxken128@users.noreply.github.com>
2023-10-22 07:32:35 +0100

    zeroize: fix feature gating and target feature cfg

--- zeroize/src/x86.rs
+++ zeroize/src/x86.rs
@@ -25,10 +25,23 @@ macro_rules! impl_zeroize_for_simd_register {
             }
         )*
     };
+
+    ($feature:expr, $($type:ty),*) => {
+        $(
+            #[cfg_attr(docsrs, doc(cfg(all(any(target_arch = "x86", target_arch = "x86_64"), feature = $feature))))]
+            impl Zeroize for $type {
+                #[inline]
+                fn zeroize(&mut self) {
+                    volatile_write(self, unsafe { core::mem::zeroed() });
+                    atomic_fence();
+                }
+            }
+        )*
+    };
 }
 
 impl_zeroize_for_simd_register!(__m128, __m128d, __m128i, __m256, __m256d, __m256i);
 
 #[cfg(feature = "avx512")]
-#[target_feature(enable = "avx512")]
-impl_zeroize_for_simd_register!(__m512, __m512d, __m512i);
+#[cfg(target_feature = "avx512")]
+impl_zeroize_for_simd_register!("avx512", __m512, __m512d, __m512i);
